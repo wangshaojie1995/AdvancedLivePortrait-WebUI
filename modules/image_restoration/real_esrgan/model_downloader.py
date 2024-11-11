1e3934e8865ec4ccaf968e2a91a88823e5871187
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-11-11 20:38:04 +0900

    Move file location

--- modules/image_restoration/real_esrgan/model_downloader.py
+++ modules/image_restoration/real_esrgan/model_downloader.py
@@ -1,8 +1,13 @@
 from modules.live_portrait.model_downloader import download_model
 
 MODELS_REALESRGAN_URL = {
-    "RealESRGAN_x2": "https://huggingface.co/ai-forever/Real-ESRGAN/resolve/main/RealESRGAN_x2.pth",
     "realesr-general-x4v3": "https://huggingface.co/jhj0517/realesr-general-x4v3/resolve/main/realesr-general-x4v3.pth",
+    "RealESRGAN_x2": "https://huggingface.co/ai-forever/Real-ESRGAN/resolve/main/RealESRGAN_x2.pth",
+}
+
+MODELS_REALESRGAN_SCALABILITY = {
+    "realesr-general-x4v3": [1, 2, 4],
+    "RealESRGAN_x2": [2]
 }
 
 
