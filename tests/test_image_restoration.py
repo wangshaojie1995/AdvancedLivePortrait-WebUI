63f92e81e149f6cc32c6e24b195f0459f767d46e
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-11-11 22:28:47 +0900

    Add test for restoration

---
+++ tests/test_image_restoration.py
@@ -0,0 +1,30 @@
+import os
+import pytest
+
+from test_config import *
+from modules.live_portrait.live_portrait_inferencer import LivePortraitInferencer
+
+
+@pytest.mark.parametrize(
+    "input_image",
+    [
+        TEST_IMAGE_PATH
+    ]
+)
+def test_image_restoration(
+    input_image: str,
+):
+    if not os.path.exists(TEST_IMAGE_PATH):
+        download_image(
+            TEST_IMAGE_URL,
+            TEST_IMAGE_PATH
+        )
+
+    inferencer = LivePortraitInferencer()
+
+    restored_output = inferencer.resrgan_inferencer.restore_image(
+        input_image
+    )
+
+    assert os.path.exists(restored_output)
+    assert are_images_different(input_image, restored_output)
